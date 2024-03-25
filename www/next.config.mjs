5ce95a85bb8b007942e76d8ee582083b4d2ce9d0
moddroid94 <ilsecco94@gmail.com>
2024-03-25 16:18:08 +0100

    backend: -fixed media url for debug frontend: -added fetch method to retrieve API from backend -added remote pattern for Images react -Added table visualization for sample list

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compiler: {
-        styledComponents: true
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: '127.0.0.1',
+                port: '8000',
+                pathname: '/media/**'
+            }
+        ]
     }
 };
 
--- www/src/app/page.js
+++ www/src/app/page.js
@@ -1,10 +1,74 @@
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import Image from 'next/image'
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Separator } from "@/components/ui/separator"
 
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('http://127.0.0.1:8000/api/sample')
+
+  return res.json()
+}
+
+export default async function Home() {
+  
+  const data = await getData()
+
   return (
     <>
-      <ScrollArea className="flex pt-16 pl-64 w-full h-screen"></ScrollArea>
+      <div className="flex transition-all pt-16 sm:pl-64 w-full h-screen">
+        <Table className=" flex flex-col overflow-scroll">
+          <TableHeader className="flex flex-row w-full h-14">
+            <TableRow className="flex flex-row w-full items-center shadow-md divide-x">
+              <TableHead className="flex flex-col w-14 h-6 pl-3
+                                    font-bold">
+                                    Pack
+              </TableHead>
+              <TableHead className="flex flex-col basis-2/5 shrink-0 h-6 
+                                    font-bold">
+                                    Name
+              </TableHead>
+              <TableHead className="flex flex-col w-16 h-6">Time</TableHead>
+              <TableHead className="flex flex-col basis-1/5 h-6">Pack</TableHead>
+              <TableHead className="flex flex-col basis-1/5 h-6">Category</TableHead>
+              
+            </TableRow>
+          </TableHeader>
+          <TableBody className="flex flex-col w-full">
+            {data.map((sample) => 
+              <TableRow key={sample.id} className="flex flex-row w-full items-center ">
+                <TableCell className="flex flex-col w-14 overflow-clip shrink-0 p-0 pl-3">
+                  <Image 
+                    className="rounded-full"
+                    src={sample.pack.cover}
+                    width={32}
+                    height={32}
+                  />
+                </TableCell>
+                <TableCell className="flex flex-col basis-2/5 shrink-0 
+                                      font-medium">
+                                      {sample.name}
+                </TableCell>
+                <TableCell className="flex flex-col basis-16">0:00</TableCell>
+                <TableCell className="flex flex-col basis-1/5">{sample.pack.name}</TableCell>
+                <TableCell className="flex flex-col basis-1/5">{sample.category_name}</TableCell>
+                
+              </TableRow>
+            )}
+          </TableBody>
+          
+        </Table>
+        
+      </div>
     </>
     
   );
