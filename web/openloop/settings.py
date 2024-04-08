fa503d5311d5a9633706692e1d07d0915823d619
0x4B1D <ilsecco94@gmail.com>
2024-04-08 07:40:33 +0200

    Update settings.py
    updated csrf-cookie allowed origins for local ip

--- web/openloop/settings.py
+++ web/openloop/settings.py
@@ -143,7 +143,7 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media Files Cookie Trust
-CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:1337", "http://192.168.1.102:1337"]
 
 # Project Specific Values
 
@@ -151,4 +151,5 @@ CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://192.168.1.102:1337"
 ]
